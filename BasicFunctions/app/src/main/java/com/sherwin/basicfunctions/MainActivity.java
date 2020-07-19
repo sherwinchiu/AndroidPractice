d45d26de76437b2040e6e7efd5f997b68bd8372e
Sherwin Chiu <sherwin.chiu89@gmail.com>
2020-07-19 17:49:43 -0400

    Added back button

--- BasicFunctions/app/src/main/java/com/sherwin/basicfunctions/MainActivity.java
+++ BasicFunctions/app/src/main/java/com/sherwin/basicfunctions/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "u the best";
+    public static final String EXTRA_MESSAGE = "com.sherwin.basicfunctions.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
