9869dc7c9fc3fedffa2f073892d8df98e6cc5dcd
Sherwin Chiu <sherwin.chiu89@gmail.com>
2020-07-21 12:12:12 -0400

    Names transfer to MainGameActivity

--- BasicFunctions/app/src/main/java/com/sherwin/basicfunctions/MainActivity.java
+++ BasicFunctions/app/src/main/java/com/sherwin/basicfunctions/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.sherwin.basicfunctions.MESSAGE";
+    public static final String PLAYER_1 = "com.sherwin.chiu.basicfunctions.P1";
+    public static final String PLAYER_2 = "com.sherwin.chiu.basicfunctions.P2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
     }
     public void startGame(View view){
         Intent intent = new Intent(this, MainGameActivity.class);
+        EditText player1EditText = (EditText) findViewById(R.id.player1EditText);
+        EditText player2EditText = (EditText) findViewById(R.id.player2EditText);
+        String player1 = player1EditText.getText().toString();
+        String player2 = player2EditText.getText().toString();
+        intent.putExtra(PLAYER_1, player1);
+        intent.putExtra(PLAYER_2, player2);
         startActivity(intent);
     }
 }
--- BasicFunctions/app/src/main/java/com/sherwin/basicfunctions/MainGameActivity.java
+++ BasicFunctions/app/src/main/java/com/sherwin/basicfunctions/MainGameActivity.java
@@ -2,7 +2,9 @@ package com.sherwin.basicfunctions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainGameActivity extends AppCompatActivity {
     final private char LENGTH = 3;
@@ -11,5 +13,16 @@ public class MainGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
+
+        Intent intent = getIntent(); // gets the intend sent to this activity
+        String player1Name = intent.getStringExtra(MainActivity.PLAYER_1); // access property of MainActivity that has EXTRA_MESSAGE contained in it
+        String player2Name = intent.getStringExtra(MainActivity.PLAYER_2);
+
+        TextView player1TextView = (TextView) findViewById(R.id.player1TextView);
+        TextView player2TextView = (TextView) findViewById(R.id.player2TextView);
+        player1TextView.setText(player1Name);
+        player2TextView.setText(player2Name);
+
+
     }
 }
